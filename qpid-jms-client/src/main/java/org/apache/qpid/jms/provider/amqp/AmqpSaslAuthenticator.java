4ba8b7d954171b357c5552203ee2960434403fad
gtully <gary.tully@gmail.com>
2017-07-20 17:55:07 +0100

    [QPIDJMS-303] implement sasl gssapi (kerberos) mechanism
    Using the current username or configured url prams the current kerberos credential
    is propagated via sasl gssapi mechanism. the default server principal is amqp/<hostName>@<REALM>

--- qpid-jms-client/src/main/java/org/apache/qpid/jms/provider/amqp/AmqpSaslAuthenticator.java
+++ qpid-jms-client/src/main/java/org/apache/qpid/jms/provider/amqp/AmqpSaslAuthenticator.java
@@ -36,6 +36,7 @@ public class AmqpSaslAuthenticator {
 
     private Mechanism mechanism;
     private boolean complete;
+    private boolean exceptionResult;
 
     /**
      * Create the authenticator and initialize it.
@@ -80,6 +81,7 @@ public class AmqpSaslAuthenticator {
             }
         } catch (JMSSecurityException result) {
             authenticationRequest.onFailure(result);
+            exceptionResult = true;
         }
 
         complete = authenticationRequest.isComplete();
@@ -92,6 +94,9 @@ public class AmqpSaslAuthenticator {
     }
 
     public boolean wasSuccessful() throws IllegalStateException {
+        if (exceptionResult) {
+            return false;
+        }
         switch (sasl.getState()) {
             case PN_SASL_CONF:
             case PN_SASL_IDLE:
@@ -137,7 +142,9 @@ public class AmqpSaslAuthenticator {
                 byte[] challenge = new byte[sasl.pending()];
                 sasl.recv(challenge, 0, challenge.length);
                 byte[] response = mechanism.getChallengeResponse(challenge);
-                sasl.send(response, 0, response.length);
+                if (response != null) {
+                    sasl.send(response, 0, response.length);
+                }
             }
         } catch (SaslException se) {
             JMSSecurityException jmsse = new JMSSecurityException("Exception while processing SASL step: " + se.getMessage());
